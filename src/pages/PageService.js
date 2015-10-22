cf8038f65fba20b97eb7e9739f1495cd3ceb7a55
alexlyte <alexanderlyte@gmail.com>
2015-10-22 17:23:00 -0400

    Update PageService.js

--- src/pages/PageService.js
+++ src/pages/PageService.js
@@ -28,7 +28,7 @@
       // },
       {
         name: 'Join',
-        avatar: 'add',
+        avatar: 'extension',
         content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
         html : './assets/partials/enroll.html'
       },
